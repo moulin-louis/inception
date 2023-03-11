a7a579112f6efdc400ad58874329ecfeeb7afecf
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-11 11:55:39 +0100

    stuff

--- srcs/wordpress/setup_wp.sh
+++ srcs/wordpress/setup_wp.sh
@@ -8,4 +8,27 @@ rm -fr latest.tar.gz
 cp -r ./wordpress/* .
 rm -rf ./wordpress
 touch /var/log/php8/www.error.log && chmod 666 /var/log/php8/www.error.log
+/usr/local/bin/wp	--info
+/usr/local/bin/wp	core download --allow-root --path="/var/www/html"
+
+rm					-f /var/www/html/wp-config.php
+cp					./wp-config.php /var/www/html/wp-config.php
+
+/usr/local/bin/wp	core install \
+					--allow-root \
+					--path="/var/www/html" \
+					--url=${WORDPRESS_HOST} \
+					--title=${WORDPRESS_TITLE} \
+					--admin_user=${WORDPRESS_ADMIN_USER} \
+					--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+					--admin_email=${WORDPRESS_ADMIN_EMAIL} \
+					--skip-email
+
+/usr/local/bin/wp	user create \
+					--allow-root \
+					--path="/var/www/html" \
+					${WORDPRESS_USER} \
+					${WORDPRESS_EMAIL} \
+					--role=author \
+					--user_pass=${WORDPRESS_PASSWORD}
 exec $@
