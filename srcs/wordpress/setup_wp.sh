d5828e89e936dcc22a471165c86d40fcee41bfb2
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-11 11:58:44 +0100

    stuff

--- srcs/wordpress/setup_wp.sh
+++ srcs/wordpress/setup_wp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p /var/www/html/wordpress
-cd /var/www/html/wordpress
+mkdir -p /var/www/html/
+cd /var/www/html/
 wget -q http://wordpress.org/latest.tar.gz
 tar -xzf latest.tar.gz
 rm -fr latest.tar.gz
@@ -10,7 +10,6 @@ rm -rf ./wordpress
 touch /var/log/php8/www.error.log && chmod 666 /var/log/php8/www.error.log
 /usr/local/bin/wp	--info
 /usr/local/bin/wp	core download --allow-root --path="/var/www/html"
-
 rm					-f /var/www/html/wp-config.php
 cp					./wp-config.php /var/www/html/wp-config.php
 
