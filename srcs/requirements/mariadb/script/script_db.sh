a092676ffced64c12d552358fbaff2afcb434dab
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-18 11:32:31 +0100

    stuff

---
+++ srcs/requirements/mariadb/script/script_db.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rc-service mariadb start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e " CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL USER 'root'@'localhost' IDENTIFIED by '${SQL_ROOT_PASSWORD}'"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
