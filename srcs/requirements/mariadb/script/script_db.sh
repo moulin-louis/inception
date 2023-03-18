86574d0a9392100cd04ed7b223c80bbecc714b04
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-18 13:12:23 +0100

    stuff

--- srcs/requirements/mariadb/script/script_db.sh
+++ srcs/requirements/mariadb/script/script_db.sh
@@ -3,4 +3,3 @@ mysql -e " CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '
 mysql -e "GRANT ALL USER 'root'@'localhost' IDENTIFIED by '${SQL_ROOT_PASSWORD}'"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-./mysqld_safe
