157dfa1d52e75b0d59533fa42c6f9f180905792e
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-18 13:17:01 +0100

    stuff

--- srcs/requirements/mariadb/script/script_db.sh
+++ srcs/requirements/mariadb/script/script_db.sh
@@ -1,3 +1,5 @@
+/etc/init.d/mariadb setup
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e " CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL USER 'root'@'localhost' IDENTIFIED by '${SQL_ROOT_PASSWORD}'"
