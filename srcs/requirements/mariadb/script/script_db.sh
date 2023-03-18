b3416cc68d3c369a1356a857d0fd6707db05978d
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-18 12:37:52 +0100

    stuff

--- srcs/requirements/mariadb/script/script_db.sh
+++ srcs/requirements/mariadb/script/script_db.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 rc-service mariadb start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e " CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
