cda9202f87460d2bea60a03f25fa56d0519d15c8
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-18 14:24:28 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -1,6 +1,19 @@
 mkdir /run/openrc && touch /run/openrc/softlevel
 
 /etc/init.d/mariadb setup
+/etc/init.d/mariadb start
+<< EOF mysql_secure_installation
+1234
+n
+Y
+$SQL_ROOT_PASSWORD
+Y
+n
+Y
+Y
+EOF
+rc-service mariadb restart
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e " CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL USER 'root'@'localhost' IDENTIFIED by '${SQL_ROOT_PASSWORD}'"
