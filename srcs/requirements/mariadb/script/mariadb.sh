48af8d500d08bd535a4179409d2e76ec8b61bf6d
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 11:25:47 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -7,13 +7,16 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 # add password to root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # shutdown the database
 mysql -e "FLUSH PRIVILEGES;"
 
 # << EOF mysqladmin -u root -p  shutdown
 # $SQL_ROOT_PASSWORD
 # EOF
+
+<< EOF mysql -uroot -p
+$SQL_ROOT_PASSWORD
+EOF
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
