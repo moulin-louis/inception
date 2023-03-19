351a9892d596c4bed91cee080fe7e02f23662117
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 12:46:38 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -9,7 +9,7 @@ until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
 done
 
 mysql << EOF
-CREATE DATABSE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 LTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
