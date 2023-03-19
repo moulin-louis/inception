19db346c1231ec4a5430f5a248316af8d2c38b83
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 17:20:18 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -1,25 +1,5 @@
 # #!/bin/sh
 
-# # try to launch mysql service
-# mysqld -u root --bind-address=localhost --silent-startup > /tmp/mariastart.log 2>&1 &
-# PID=$!
-
-# until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
-# 	sleep 0.2
-# done
-
-# mysql << EOF
-# CREATE DATABASE \`${SQL_DATABASE}\`;
-# GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-# GRANT USAGE ON \`${SQL_DATABASE}\`.* TO '$WP_USER_LOGIN'@'localhost' IDENTIFIED BY '$WP_USER_PASSWORD';
-# FLUSH PRIVILEGES;
-# EOF
-
-# kill -TERM ${PID}
-# sleep 3
-# exec $@
-
 mysqld -u root --bind-address=localhost --silent-startup > /tmp/mariastart.log 2>&1 &
 PID=$!
 until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
@@ -37,14 +17,3 @@ kill -TERM ${PID}
 chown -R mysql:mysql /var/lib/mysql;
 sleep 2
 exec $@
-
-# << EOF mysqladmin -u root -p  shutdown
-# $SQL_ROOT_PASSWORD
-# EOF
-
-# /etc/init.d/mysql stop
-
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-# relaunch the service
-# exec mysqld_safe
