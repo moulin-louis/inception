5ac19c577ebb9ff9d79e48fad61f2b03fd2b1c43
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 12:55:33 +0100

    stuff

--- srcs/requirements/wordpress/script/setup_wp.sh
+++ srcs/requirements/wordpress/script/setup_wp.sh
@@ -1,18 +1,15 @@
 #!bin/sh
 sleep 10
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-fi
-
+rm -rf /var/www/wordpress/wp-config.php
+wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
+          --dbhost=mariadb:3306 --path='/var/www/wordpress'
 sleep 2
 wp core install --url=loumouli.42.fr --title=Inception-42\
   --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD\
   --admin_email=$WP_ADMIN_MAIL --allow-root --path='/var/www/wordpress'
 
 wp user create --allow-root --role=author $WP_USER_LOGIN $WP_USER_MAIL\
- --user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
-fi
+--user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
 
 if [ ! -d "/run/php" ]; then
   mkdir /run/php
