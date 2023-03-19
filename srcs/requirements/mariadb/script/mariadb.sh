eab6137c26bf30ccd9d0df2d0c638a542a13bcef
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 15:02:44 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -1,23 +1,42 @@
-#!/bin/sh
+# #!/bin/sh
 
-# try to launch mysql service
+# # try to launch mysql service
+# mysqld -u root --bind-address=localhost --silent-startup > /tmp/mariastart.log 2>&1 &
+# PID=$!
+
+# until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
+# 	sleep 0.2
+# done
+
+# mysql << EOF
+# CREATE DATABASE \`${SQL_DATABASE}\`;
+# GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+# GRANT USAGE ON \`${SQL_DATABASE}\`.* TO '$WP_USER_LOGIN'@'localhost' IDENTIFIED BY '$WP_USER_PASSWORD';
+# FLUSH PRIVILEGES;
+# EOF
+
+# kill -TERM ${PID}
+# sleep 3
+# exec $@
+
 mysqld -u root --bind-address=localhost --silent-startup > /tmp/mariastart.log 2>&1 &
 PID=$!
-
 until tail "/tmp/mariastart.log" | grep -qi "Version:"; do
 	sleep 0.2
 done
 
-mysql << EOF
-CREATE DATABASE \`${SQL_DATABASE}\`;
-GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-GRANT USAGE ON \`${SQL_DATABASE}\`.* TO '$WP_USER_LOGIN'@'localhost' IDENTIFIED BY '$WP_USER_PASSWORD';
-FLUSH PRIVILEGES;
+mysql <<EOF
+create database \`${SQL_DATABASE}\`;
+grant all privileges on \`${SQL_DATABASE}\`.* to  \`${SQL_USER}\`@'%' identified by '${SQL_PASSWORD}';
+alter user 'root'@'localhost' identified by '${SQL_ROOT_PASSWORD}';
+grant usage on \`${SQL_DATABASE}\`.* to '${WP_USER_LOGIN}'@'localhost' identified by '${WP_USER_PASSWORD}';
+flush privileges;
 EOF
 
 kill -TERM ${PID}
-sleep 3
+chown -R mysql:mysql /var/lib/mysql;
+
 exec $@
 
 # << EOF mysqladmin -u root -p  shutdown
