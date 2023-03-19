ef3f6fc9a7b77af8069a1ea7c920d59bf394125c
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 12:19:32 +0100

    stuff

--- srcs/requirements/mariadb/script/mariadb.sh
+++ srcs/requirements/mariadb/script/mariadb.sh
@@ -8,12 +8,14 @@ mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 # add password to root
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# shutdown the database
+mysql -e "FLUSH PRIVILEGES;"
 
-# << EOF mysqladmin -uroot -p shutdown
+# << EOF mysqladmin -u root -p  shutdown
 # $SQL_ROOT_PASSWORD
 # EOF
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 # relaunch the service
-# exec mysqld_safe
+exec mysqld_safe
--- srcs/requirements/wordpress/script/setup_wp.sh
+++ srcs/requirements/wordpress/script/setup_wp.sh
@@ -1,5 +1,5 @@
 #!bin/sh
-sleep 5
+sleep 10
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
