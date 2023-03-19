2ce92caaf2914c42f157588c054969100651bb17
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 12:53:33 +0100

    stuff

--- srcs/requirements/wordpress/script/setup_wp.sh
+++ srcs/requirements/wordpress/script/setup_wp.sh
@@ -3,6 +3,7 @@ sleep 10
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+fi
 
 sleep 2
 wp core install --url=loumouli.42.fr --title=Inception-42\
