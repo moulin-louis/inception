eec912d3c8cc3334181ed8f8adef8b4690bf3e2c
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 11:36:57 +0100

    stuff

--- srcs/requirements/wordpress/script/setup_wp.sh
+++ srcs/requirements/wordpress/script/setup_wp.sh
@@ -1,5 +1,5 @@
 #!bin/sh
-sleep 10
+sleep 5
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
