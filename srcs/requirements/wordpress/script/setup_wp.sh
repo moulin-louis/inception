1793dc1d23852fbb4f32919253d4d3cb55578631
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-03-19 15:33:07 +0100

    stuff

--- srcs/requirements/wordpress/script/setup_wp.sh
+++ srcs/requirements/wordpress/script/setup_wp.sh
@@ -9,15 +9,15 @@ wp config create --allow-root\
   --path='/var/www/wordpress'
 sleep 2
 
-wp core install --url=loumouli.42.fr --title="Inception-42"\
-  --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD\
-  --admin_email=$WP_ADMIN_MAIL --allow-root --path='/var/www/wordpress'
+# wp core install --url=loumouli.42.fr --title="Inception-42"\
+#   --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD\
+#   --admin_email=$WP_ADMIN_MAIL --allow-root --path='/var/www/wordpress'
 
 # wp user create --allow-root --role=author $WP_USER_LOGIN $WP_USER_MAIL\
 #  --user_pass=$WP_USER_PASSWORD --path='/var/www/wordpress' >> /log.txt
 
-if [ ! -d "/run/php" ]; then
-  mkdir /run/php
-fi
+# if [ ! -d "/run/php" ]; then
+#   mkdir /run/php
+# fi
 
-exec $@
+# exec $@
